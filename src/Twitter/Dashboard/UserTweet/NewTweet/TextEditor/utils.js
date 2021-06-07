09b232e90ea98d4eed8d2d6278930bf45dbf7439
Le Duy Hien <d-le@usen-next.jp>
2021-06-07 11:01:31 +0700

    refactor code

--- src/Twitter/Dashboard/UserTweet/NewTweet/TextEditor/utils.js
+++ src/Twitter/Dashboard/UserTweet/NewTweet/TextEditor/utils.js
@@ -1,23 +1,3 @@
-export const getTextValue = (element) => {
-  if (!element) return "";
-  if (element.nodeType === Node.TEXT_NODE) return element.nodeValue;
-  if (!element.childNodes) return "";
-  let value = "";
-  Array.from(element.childNodes).forEach((node) => {
-    switch (node.nodeType) {
-      case Node.TEXT_NODE:
-        value += node.nodeValue;
-        break;
-      case Node.ELEMENT_NODE:
-        value += getTextValue(node);
-        break;
-      default:
-        throw new Error(`Unexpected node type: ${node.nodeType}`);
-    }
-  });
-  return value;
-};
-
 export const getTextSegments = (element) => {
   const textSegments = [];
   Array.from(element.childNodes).forEach((node) => {
